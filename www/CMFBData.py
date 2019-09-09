d095c8056a942984bf2590d750cb06244de8e87c
coolwater1 <linux_wang@hotmail.com>
2019-09-10 00:15:00 +0800

    upate 20190910

--- www/CMFBData.py
+++ www/CMFBData.py
@@ -17,22 +17,20 @@ from fake_useragent import UserAgent
 import pyautogui
 
 class CMBFData(object):
-    def __init__(self,silent=True):
+    def __init__(self, load_parameter='silent'):
         print(self.__class__.__name__+" __init__")
         # 给浏览器设置属性
         option = webdriver.ChromeOptions()
         # 隐藏警告语‘Chrome正在受到自动软件的控制’
         option.add_argument('disable-infobars')
         # 设置隐藏还是前台运行,默认隐藏
-        if silent:
+        if load_parameter == 'silent':
             option.add_argument("headless")
         # 产生随机user-agent
         option.add_argument(UserAgent().random)
         self.browser = webdriver.Chrome(options=option)
 
-        # 显示打开chrome浏览器（需提前安装好chromedriver）
-        self.browser = webdriver.Chrome()
-        if not silent:
+        if not (load_parameter == 'silent'):
             #最大化浏览器
             self.browser.maximize_window()
             
@@ -135,35 +133,14 @@ class CMBFData(object):
         return cmfb_data
 
 
-    def _get_data_current(self):
-        cmfb_data = {}
-
-        # 这里经常出现加载超的异常，后面需要处理一下：捕获异常后，刷新浏览器
-        browser = self.browser
-        browser.get(url)
-
-        # 找到筹码分布的按钮---通过xpath
-        btn_cmfb_xpath = "//a[text()='筹码分布']"
-        # 等待响应完成
-        wait = WebDriverWait(browser, 10)
-        wait.until(EC.presence_of_element_located((By.XPATH, btn_cmfb_xpath)))
-        # 查找目标按钮
-        btn_cmfb = browser.find_element_by_xpath(btn_cmfb_xpath)
-        # 找到按钮后单击
-        btn_cmfb.click()
-
-        # 等待筹码分布的元素显示出来，不然解析数据的时候抓取不到相关数据
-        wait = WebDriverWait(browser, 10)
-        # wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='__emchatrs3_cmfb']" )))
-        # wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='__emchatrs3_cmfb']" )))
-        wait.until(EC.text_to_be_present_in_element((By.XPATH,"//div[@class='__emchatrs3_cmfb']"),u'集中度'))
-
+    # 解析网页获取筹码分布数据并返回数据
+    def _get_cmfb_data(self):
         COLUMN_LIST = ('日期','获利比例','亏损比例','平均成本','90%成本','90成本集中度','70%成本','70成本集中度')
         count = len(COLUMN_LIST)
         data = {}
         index = 0
         # 解析网页
-        soup = BeautifulSoup(browser.page_source, 'lxml')
+        soup = BeautifulSoup(self.browser.page_source, 'lxml')
         for span in soup.find(class_="__emchatrs3_cmfb").find_all('span'):
             if index >= count:
                 break
@@ -173,52 +150,159 @@ class CMBFData(object):
             index = index + 1
 
         print(data)
-        cmfb_data[date] = data
+        return data
+        
 
-        print(len(cmfb_data))
-        print(cmfb_data)
-        return cmfb_data
-
-    def getUrl(self, code):
+    def _get_url(self, code):
         codelist = {'60': 'sh', '00':'sz', '30':'sz'}
         for item in codelist:
             if code.startswith(item):
                 return "http://quote.eastmoney.com/concept/" + codelist[item] + code + ".html"
 
-    def getDataInternel(self, code):
-        try:
-            time.sleep(random.randint(0,3)+random.random()) #随机延时?.?s  以防封IP
-            url = self.getUrl(code)
-            data = self.getDataFromWeb(url)
-            # code
-            print("股票代码: " + code)
-            # print(data)
-        except TimeoutException:
-            self.retrytime = self.retrytime - 1
-            print(code + " retrytime left " + str(self.retrytime))
-            if self.retrytime > 0 : 
-                self.getDataInternel(code)
-            else:
-                self.timeoutCodeList.append(code)
-        finally:
-            pass
+    # 打开并加载网页，等待完成
+    def _load_web(self, url):
+        # 这里经常出现加载超的异常，后面需要处理一下：捕获异常后，刷新浏览器
+        retry_time = 3
+        while True:
+            if retry_time == 0:
+                return False
+            try:
+                browser = self.browser
+                browser.get(url)
+                # 找到筹码分布的按钮---通过xpath
+                btn_cmfb_xpath = "//a[text()='筹码分布']"
+                # 等待响应完成
+                wait = WebDriverWait(browser, 10)
+                wait.until(EC.presence_of_element_located((By.XPATH, btn_cmfb_xpath)))
+                # 查找目标按钮
+                btn_cmfb = browser.find_element_by_xpath(btn_cmfb_xpath)
+                # 找到按钮后单击
+                btn_cmfb.click()
+
+                # 等待筹码分布的元素显示出来，不然解析数据的时候抓取不到相关数据
+                wait = WebDriverWait(browser, 10)
+                # wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='__emchatrs3_cmfb']" )))
+                # wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='__emchatrs3_cmfb']" )))
+                wait.until(EC.text_to_be_present_in_element((By.XPATH,"//div[@class='__emchatrs3_cmfb']"),u'集中度'))
+                return True
+            except TimeoutException:
+                retry_time = retry_time - 1
+                continue
+
+    def get_data_current(self,code):
+        data_list = []
+
+        url = self._get_url(code)
+        # 如果网页加载失败,直接返回
+        if not self._load_web(url):
+            print("网页加载失败: " + url)
+            return data_list
+        
+        data = self._get_cmfb_data()
+        data_list.append(data)
+
+        print(data_list)
+        return data_list
+
+    def get_data_history(self,code):
+        data_list = []
+
+        url = self._get_url(code)
+        # 如果网页加载失败,直接返回
+        if not self._load_web(url):
+            print("网页加载失败: " + url)
+            return data_list
+        
+        browser = self.browser
+        # 移动滚动条定位到某个元素，使这个元素在可见区域，一般在最顶上
+        target = browser.find_element_by_xpath("//div[@class='kr-box']")
+        # target = browser.find_element_by_xpath(btn_cmfb_xpath)
+        browser.execute_script("arguments[0].scrollIntoView();", target)
+
+        time.sleep(2)
+        # 移动到某个起始位置
+        START_X = 0
+        START_Y = 0 
+        END_X = 0
+        MOVE_X = 0
+        screenWidth,screenHeight = pyautogui.size()
+        print("screenWidth : " + str(screenWidth))
+        if screenWidth == 1366 :
+            START_X = 350
+            END_X = 996
+            START_Y = 485
+            MOVE_X = 8
+        elif screenWidth == 1920:
+            START_X = 544    
+            END_X = 1350
+            START_Y = 666
+            MOVE_X = 10
+        else:
+            print("不能匹配到屏幕尺寸，请增加")
+            return
+        currentX= START_X
+        currentY= START_Y
+
+        pyautogui.moveTo(START_X, START_Y)
+        time.sleep(2)
+
+        while currentX < END_X:
+            data = self._get_cmfb_data()
+            data_list.append(data)
+
+            # #  鼠标向右移动x像素
+            currentX = currentX + MOVE_X
+            pyautogui.moveTo(currentX, currentY)
+            # 等待筹码分布的元素显示出来，不然解析数据的时候抓取不到相关数据
+            wait = WebDriverWait(browser, 10)
+            # wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='__emchatrs3_cmfb']" )))
+            # wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='__emchatrs3_cmfb']" )))
+            wait.until(EC.text_to_be_present_in_element((By.XPATH,"//div[@class='__emchatrs3_cmfb']"),u'集中度'))
+
+        # data_list 需要去重和排序
+        print(data_list)
+        return data_list
+
+    # def getDataInternel(self, code):
+    #     try:
+    #         time.sleep(random.randint(0,3)+random.random()) #随机延时?.?s  以防封IP
+    #         url = self._get_url(code)
+    #         data = self.getDataFromWeb(url)
+    #         # code
+    #         print("股票代码: " + code)
+    #         # print(data)
+    #     except TimeoutException:
+    #         self.retrytime = self.retrytime - 1
+    #         print(code + " retrytime left " + str(self.retrytime))
+    #         if self.retrytime > 0 : 
+    #             self.getDataInternel(code)
+    #         else:
+    #             self.timeoutCodeList.append(code)
+    #     finally:
+    #         pass
     
-    def getData(self, code, retrytime=3):
-        self.retrytime = retrytime
-        self.getDataInternel(code, "history")
+    # def getData(self, code, retrytime=3):
+    #     self.retrytime = retrytime
+    #     self.getDataInternel(code, "history")
 
-    def get_current(self,code, retrytime=3):
-        self.retrytime = retrytime
-        self.getDataInternel(code, "current")
+    # def get_current(self,code, retrytime=3):
+    #     self.retrytime = retrytime
+    #     self.getDataInternel(code, "current")
 
-    def get_history(self):
+    # def get_history(self):
+    #     pass
 
 
 
 def main():
     # http://quote.eastmoney.com/concept/sz000002.html
-    test = CMBFData(False)
-    test.getData('000002')
+    # test = CMBFData()
+    # test.get_data_current('000002')
+
+    # 抓取历史数据必须打开浏览器到前台
+    test = CMBFData('show_browser')
+    test.get_data_history('000002')
+    # test.getData('000002')
     # test.getData('601318')
     # test.getData('300002')
  
